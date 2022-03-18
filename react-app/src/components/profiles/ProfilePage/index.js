47b6b406153139fbe561043688e373b20e8f3848
David Alliger <alligerdavidj@gmail.com>
2022-03-18 12:50:26 -0500

    Added styling for profile page

--- react-app/src/components/profiles/ProfilePage/index.js
+++ react-app/src/components/profiles/ProfilePage/index.js
@@ -14,22 +14,24 @@ const ProfilePage = () => {
         if (profile.userId === +id) profileMatch = profile;
         return profileMatch;
     }, null);
-    // const history = useHistory();
-    const [showModal, setShowModal] = useState(false)
-
+    const [showModal, setShowModal] = useState(false);
     const handleDelete = () => {
         setShowModal(true);
     }
-    console.log(user.profileId);
+
     if (!userProfile) {
         return <Redirect to='/users'/>
     }
+
 
     return (
         <div id='profile-page'>
             <div id='profile-page-card'>
                 <div id='profile-page-heading'>
-                    <div id='profile-page-heading-image'>
+                    <div id='profile-page-heading-image'
+                        // onMouseEnter={handleHover}
+                        // onMouseLeave={handleLeave}
+                    >
                         {userProfile.imgUrl && (
                             <div className='profile-page-user-image' style={{backgroundImage: `url(${userProfile.imgUrl})`}} />
                             )}
@@ -37,10 +39,11 @@ const ProfilePage = () => {
                             <i className="fa-solid fa-circle-user profile-page-user-no-image"></i>
                         )}
                     </div>
+                    <div id='profile-page-edit-picture'>
+                        <i className="fa-solid fa-camera"></i>
+                        <p id='profile-page-edit-picture-text'>Update Image</p>
+                    </div>
                     <div id='profile-page-heading-info-containter'>
-                        {/* {(user.id === +id) && (
-                            <div className='profile-page-edit-button'><i class="fa-solid fa-pen-to-square"></i></div>
-                        )} */}
                         <div id='profile-page-heading-info'>
                             <div id='profile-page-username'>
                                 <h1>{userProfile.username}</h1>
@@ -48,8 +51,8 @@ const ProfilePage = () => {
                             <div id='profile-page-location'>
                                 <p>
                                     {userProfile.state && (
-                                    <span>{userProfile.state}, </span>
-                                    )}
+                                        <span>{userProfile.state}, </span>
+                                        )}
                                     {userProfile.country}
                                 </p>
                             </div>
@@ -62,6 +65,9 @@ const ProfilePage = () => {
                                 </div>
                             )}
                         </div>
+                        {(user.id === +id) && (
+                            <div className='profile-page-edit-button'><i class="fa-solid fa-pen-to-square"></i></div>
+                        )}
                     </div>
                 </div>
                 <div id='profile-page-lower'>
@@ -70,9 +76,9 @@ const ProfilePage = () => {
                             <h3>About Me</h3>
                             {userProfile.about}
                         </div>
-                        {/* {(user.id === +id) && (
-                            <button className='profile-page-edit-button'>Edit</button>
-                        )} */}
+                        {(user.id === +id) && (
+                            <div className='profile-page-edit-button'><i class="fa-solid fa-pen-to-square"></i></div>
+                        )}
                     </div>
                 </div>
             </div>
