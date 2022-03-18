51fd35353776e37d01a3d1c7a5927cd065ecc9ac
David Alliger <alligerdavidj@gmail.com>
2022-03-18 02:09:37 -0500

    Added styling to the landing page.

--- react-app/src/components/home/LandingPage/index.js
+++ react-app/src/components/home/LandingPage/index.js
@@ -2,10 +2,15 @@ import { useEffect, useState } from "react";
 import { useSelector } from "react-redux";
 import SignUpModal from "../../auth/signup/SignUpModal";
 import ProfilesFeed from "../../profiles/ProfilesFeed";
+import './LandingPage.css'
 
 const LandingPage = () => {
     const [showModal, setShowModal] = useState(false);
     const user = useSelector(state => state.session.user);
+
+    // useEffect (() => {
+    //     document.getElementById('root').classList.add('landing-page-root');
+    // }, [])
 
     if (user) {
         return (
@@ -13,11 +18,36 @@ const LandingPage = () => {
         )
     }
 
+    const landingPageImg = '../../../../images/landing-page.jpg';
+
     return (
-        <div>
-            <h1>Landing Page</h1>
-            <button onClick={() => setShowModal(true)}>Get Started</button>
-            <SignUpModal showModal={showModal} setShowModal={setShowModal} />
+        <div id='landing-page'>
+            <div id='landing-page-card'>
+                <div id='landing-page-intro-text-div'>
+                    <h1>Become fluent in any language</h1>
+                    <div id='landing-page-list'>
+                        <p id='landing-page-list-1'>
+                            Bullet Point
+                        </p>
+                        <p id='landing-page-list-2'>
+                            Bullet Point
+                        </p>
+                        <p id='landing-page-list-3'>
+                            Bullet Point
+                        </p>
+                    </div>
+                    <button id='landing-page-get-started' onClick={() => setShowModal(true)}>Get Started</button>
+                    <SignUpModal showModal={showModal} setShowModal={setShowModal} />
+                </div>
+                <div id='landing-page-img-div'>
+                    <div id='landing-page-img' style={{backgroundImage: `url(${landingPageImg})`}}>
+                    </div>
+                </div>
+            </div>
+            <div id='attribution-link'>
+                <a  href="https://www.freepik.com/vectors/people">People vector created by pikisuperstar - www.freepik.com</a>
+            </div >
+
         </div>
     );
 
